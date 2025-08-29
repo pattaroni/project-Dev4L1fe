c21b2767603ef498f2fc1543b181414e9ba9f550
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-08-29 19:00:06 +0300

    add render artists

---
+++ src/js/handlers.js
@@ -0,0 +1,18 @@
+import iziToast from 'izitoast';
+import { fetchArtists } from './api';
+import { renderArtists } from './render-function';
+
+export async function handleArtists() {
+  try {
+    const { data } = await fetchArtists();
+    renderArtists(data.artists);
+  } catch (error) {
+    if (error.response) {
+      iziToast.error({
+        message: `Error ${error.response.status}: ${error.response.data}`,
+      });
+    } else {
+      iziToast.error({ message: `Error: ${error.message}` });
+    }
+  }
+}
