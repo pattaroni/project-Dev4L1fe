3ab3864e4da6c8f7fbe59f0632a6056f588f490f
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2025-08-29 11:39:40 +0300

    feat: Made fetch functions and API constants.

---
+++ src/js/api.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { api } from './constants';
+
+const { BASE_URL, ENDPOINTS, PER_PAGE } = api;
+
+axios.defaults.baseURL = BASE_URL;
+
+export const fetchGenres = async () => {
+  const { data } = await axios.get(ENDPOINTS.genres());
+  return data;
+};
+
+// use SORT_TYPES from constants.js for sortName
+export const fetchArtists = async ({
+  page = 1,
+  genre,
+  name,
+  sortName,
+} = {}) => {
+  const { data } = await axios.get(ENDPOINTS.artists(), {
+    params: {
+      page,
+      limit: PER_PAGE,
+      ...(!!genre && { genre }),
+      ...(!!name && { name }),
+      ...(!!sortName && { sortName }),
+    },
+  });
+
+  const totalItems = data.totalArtists || 0;
+  const isLastPage = page >= Math.ceil(totalItems / PER_PAGE);
+
+  return { data, isLastPage };
+};
+
+export const fetchArtistById = async id => {
+  const { data } = await axios.get(ENDPOINTS.artistById(id));
+  return data;
+};
+
+export const fetchArtistByIdWithAlbums = async id => {
+  const { data } = await axios.get(ENDPOINTS.artistByIdWithAlbums(id));
+  return data;
+};
+
+export const postFeedback = async feedback => {
+  const { data } = await axios.post(ENDPOINTS.feedbacks(), feedback);
+  return data;
+};
+
+export const fetchFeedbacks = async (page = 1) => {
+  const { data } = await axios.get(ENDPOINTS.feedbacks(), {
+    params: {
+      page,
+      limit: PER_PAGE,
+    },
+  });
+
+  const totalItems = data.total || 0;
+  const isLastPage = page >= Math.ceil(totalItems / PER_PAGE);
+
+  return { data, isLastPage };
+};
