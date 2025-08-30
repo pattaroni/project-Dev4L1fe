43cc0328d68cf7c127343efeeb117ca2f1dcc8cf
pattaroni <105497306+pattaroni@users.noreply.github.com>
2025-08-30 11:46:10 +0000

    Deploying to gh-pages from @ pattaroni/project-Dev4L1fe@fed076eff409c48f2f959e94a78f0ecc38ce0005 🚀

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import{a as l,i as n}from"./assets/vendor-BO1i2NM4.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const o of e.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function r(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function a(t){if(t.ep)return;t.ep=!0;const e=r(t);fetch(t.href,e)}})();const u={BASE_URL:"https://sound-wave.b.goit.study/api",ENDPOINTS:{genres:()=>"/genres",artists:()=>"/artists",artistById:s=>`/artists/${s}`,artistByIdWithAlbums:s=>`/artists/${s}/albums`,feedbacks:()=>"/feedbacks"},PER_PAGE:8},{BASE_URL:d,ENDPOINTS:f,PER_PAGE:c}=u;l.defaults.baseURL=d;const p=async({page:s=1,genre:i,name:r,sortName:a}={})=>{const{data:t}=await l.get(f.artists(),{params:{page:s,limit:c,...!!i&&{genre:i},...!!r&&{name:r},...!!a&&{sortName:a}}}),e=t.totalArtists||0,o=s>=Math.ceil(e/c);return{data:t,isLastPage:o}},m={artistsList:document.querySelector(".artists-list")};function g(s){const i=s.map(r=>{const a=r.genres.map(t=>`<li class="genres-list-item">${t}</li>`).join("");return`<ul class="artists-list">
-            <li class="artists-list-item">
-                <img src="${r.strArtistThumb}" alt="${r.strArtist}" />
+import{a as l,i as a}from"./assets/vendor-BO1i2NM4.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const e of s)if(e.type==="childList")for(const n of e.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function r(s){const e={};return s.integrity&&(e.integrity=s.integrity),s.referrerPolicy&&(e.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?e.credentials="include":s.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function o(s){if(s.ep)return;s.ep=!0;const e=r(s);fetch(s.href,e)}})();const u={BASE_URL:"https://sound-wave.b.goit.study/api",ENDPOINTS:{genres:()=>"/genres",artists:()=>"/artists",artistById:t=>`/artists/${t}`,artistByIdWithAlbums:t=>`/artists/${t}/albums`,feedbacks:()=>"/feedbacks"},PER_PAGE:8},{BASE_URL:p,ENDPOINTS:d,PER_PAGE:c}=u;l.defaults.baseURL=p;const f=async({page:t=1,genre:i,name:r,sortName:o}={})=>{const{data:s}=await l.get(d.artists(),{params:{page:t,limit:c,...!!i&&{genre:i},...!!r&&{name:r},...!!o&&{sortName:o}}}),e=s.totalArtists||0,n=t>=Math.ceil(e/c);return{data:s,isLastPage:n}},m={artistsList:document.querySelector(".artists-list")},g=(t="")=>CSS.supports("-webkit-line-clamp","2")&&CSS.supports("display","-webkit-box")?t:t.length>80?t.slice(0,80).trim()+"...":t;function h(t){const i=t.map(r=>{const o=r.genres.map(e=>`<li class="genres-list-item">${e}</li>`).join(""),s=g(r.strBiographyEN);return`<li class="artists-list-item">
+                <img class="artist-image"
+                  src="${r.strArtistThumb}" 
+                  alt="${r.strArtist}" />
                 <ul class="genres-list">
-                    ${a}
+                    ${o}
                 </ul>
                 <h3 class="artist-name">${r.strArtist}</h3>
-                <p class="artist-descr">${r.strBiographyEN.split(".")[0]+"."}</p>
+                <p class="artist-descr">${s}</p>
                 <button type="button" class="artist-btn-learn-more">
                     <span>Learn More</span>
                     <span>
-                        <svg class="artist-learn-svg" width="24" height="24">
+                        <svg class="artist-learn-svg" width="14" height="14">
                             <use href="/img/sprite.svg#next-icon"></use>
                         </svg>
                     </span>
                 </button>
-            </li>
-        </ul>`});m.artistsList.insertAdjacentHTML("beforeend",i)}async function h(){try{const{data:s}=await p();console.log(s),g(s.artists)}catch(s){s.response?n.error({message:`Error ${s.response.status}: ${s.response.data}`}):n.error({message:`Error: ${s.message}`})}}document.addEventListener("DOMContentLoaded",h());
+            </li>`}).join("");m.artistsList.insertAdjacentHTML("beforeend",i)}async function y(){try{const{data:t}=await f();console.log(t),h(t.artists)}catch(t){t.response?a.error({message:`Error ${t.response.status}: ${t.response.data}`}):a.error({message:`Error: ${t.message}`})}}document.addEventListener("DOMContentLoaded",y());
 //# sourceMappingURL=index.js.map
