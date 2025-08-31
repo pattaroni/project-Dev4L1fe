0315346a81f36e637a66d8a0714f6539a42a69a5
jres <ljresetl@gmail.com>
2025-08-31 14:30:55 +0200

    header finish

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { handleArtists } from './js/handlers';
 import { loader } from './js/helpers.js';
 import { refs } from './js/refs.js';
 import './js/to-top.js';
+import './js/mobil-menu.js';
 
 handleArtists();
