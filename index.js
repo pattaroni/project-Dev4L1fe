b07359f8fb4e4c398c6ced5ec9f0016a9dfcaf90
pattaroni <105497306+pattaroni@users.noreply.github.com>
2025-08-31 12:57:51 +0000

    Deploying to gh-pages from @ pattaroni/project-Dev4L1fe@d90ee13892b01942ffd5cd648bd1ea7d8966c1d1 🚀

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import{a as p,i as l}from"./assets/vendor-BO1i2NM4.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function i(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=i(t);fetch(t.href,r)}})();const f={BASE_URL:"https://sound-wave.b.goit.study/api",ENDPOINTS:{genres:()=>"/genres",artists:()=>"/artists",artistById:s=>`/artists/${s}`,artistByIdWithAlbums:s=>`/artists/${s}/albums`,feedbacks:()=>"/feedbacks"},PER_PAGE:8},{BASE_URL:m,ENDPOINTS:h,PER_PAGE:d}=f;p.defaults.baseURL=m;const g=async({page:s=1,genre:e,name:i,sortName:o}={})=>{const{data:t}=await p.get(h.artists(),{params:{page:s,limit:d,...!!e&&{genre:e},...!!i&&{name:i},...!!o&&{sortName:o}}}),r=t.totalArtists||0,a=s>=Math.ceil(r/d);return{data:t,isLastPage:a}},u={artistsList:document.querySelector(".artists-list"),loader:document.querySelector(".loader")},y=(s="")=>CSS.supports("-webkit-line-clamp","2")&&CSS.supports("display","-webkit-box")?s:s.length>80?s.slice(0,80).trim()+"...":s,n={create(s){return s?(s.insertAdjacentHTML("beforeend",'<span class="loader hidden"></span>'),s.querySelector(".loader")):void 0},show(s){s?.classList.remove("hidden")},hide(s){s?.classList.add("hidden")}},L="/project-Dev4L1fe/assets/sprite-DpWu6vF8.svg";function b(s){const e=s.map(i=>{const o=i.genres.map(r=>`<li class="genres-list-item">${r}</li>`).join(""),t=y(i.strBiographyEN);return`<li class="artists-list-item" data-id="${i._id}">
+import{a as f,S as w,i as v}from"./assets/vendor-F9iMMrrb.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function i(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=i(t);fetch(t.href,r)}})();const L={BASE_URL:"https://sound-wave.b.goit.study/api",ENDPOINTS:{genres:()=>"/genres",artists:()=>"/artists",artistById:e=>`/artists/${e}`,artistByIdWithAlbums:e=>`/artists/${e}/albums`,feedbacks:()=>"/feedbacks"},PER_PAGE:8},{BASE_URL:S,ENDPOINTS:b,PER_PAGE:h}=L;f.defaults.baseURL=S;const E=async({page:e=1,genre:s,name:i,sortName:n}={})=>{const{data:t}=await f.get(b.artists(),{params:{page:e,limit:h,...!!s&&{genre:s},...!!i&&{name:i},...!!n&&{sortName:n}}}),r=t.totalArtists||0,a=e>=Math.ceil(r/h);return{data:t,isLastPage:a}},k=async(e=1,s=10)=>{const{data:i}=await f.get(b.feedbacks(),{params:{page:e,limit:s}}),n=i.data||[],t=i.total||0,r=e>=Math.ceil(t/s);return{feedbacks:n,isLastPage:r}},g={artistsList:document.querySelector(".artists-list"),containerEL:document.querySelector(".container"),loader:document.querySelector(".loader")},A=(e="")=>CSS.supports("-webkit-line-clamp","2")&&CSS.supports("display","-webkit-box")?e:e.length>80?e.slice(0,80).trim()+"...":e,o={create(e){return e?(e.insertAdjacentHTML("beforeend",'<span class="loader hidden"></span>'),e.querySelector(".loader")):void 0},show(e){e?.classList.remove("hidden")},hide(e){e?.classList.add("hidden")}},u="/project-Dev4L1fe/assets/sprite-DpWu6vF8.svg";function $(e){const s=e.map(i=>{const n=i.genres.map(r=>`<li class="genres-list-item">${r}</li>`).join(""),t=A(i.strBiographyEN);return`<li class="artists-list-item" data-id="${i._id}">
                 <img class="artist-image"
                   src="${i.strArtistThumb}" 
                   alt="${i.strArtist}" />
                 <ul class="genres-list">
-                    ${o}
+                    ${n}
                 </ul>
                 <h3 class="artist-name">${i.strArtist}</h3>
                 <p class="artist-descr">${t}</p>
@@ -11,9 +11,38 @@ import{a as p,i as l}from"./assets/vendor-BO1i2NM4.js";(function(){const e=docum
                     <span>Learn More</span>
                     <span>
                         <svg class="artist-learn-svg" width="14" height="14">
-                          <use href="${L}#next-icon"></use>
+                          <use href="${u}#next-icon"></use>
                         </svg>
                     </span>
                 </button>
-            </li>`}).join("");u.artistsList.insertAdjacentHTML("beforeend",e)}async function w(){let s;try{s=n.create(u.artistsList),n.show(s);const{data:e}=await g();n.show(s),b(e.artists)}catch(e){e.response?l.error({message:`Error ${e.response.status}: ${e.response.data}`}):l.error({message:`Error: ${e.message}`})}finally{n.hide(s)}}const c=document.getElementById("toTop");window.addEventListener("scroll",()=>{window.scrollY>600?c.classList.add("show"):c.classList.remove("show")});c.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});w();
+            </li>`}).join("");g.artistsList.insertAdjacentHTML("beforeend",s)}const P=e=>{const s=document.querySelector(".section-feedback .container");if(!s)return console.error("Container not found");const i=`
+    <div class="feedback-slider swiper">
+      <div class="swiper-wrapper">
+        ${e.map(a=>`
+          <div class="swiper-slide">
+            <div class="feedback-card">
+              <div class="stars">${q(a.rating)}</div>
+              <p class="comment">"${a.descr}"</p>
+              <h3 class="comm-name">${a.name}</h3>
+            </div>
+          </div>
+        `).join("")}
+      </div>
+      <div class="swiper-button-prev">
+        <svg class="icon">
+          <use href="${u}#left-arrow-icon"></use>
+        </svg>
+      </div>
+      <div class="swiper-button-next">
+        <svg class="icon">
+          <use href="${u}#right-arrow-icon"></use>
+        </svg>
+      </div>
+      <div class="custom-pagination">
+        <span class="bullet bullet-left"></span>
+        <span class="bullet bullet-middle"></span>
+        <span class="bullet bullet-right"></span>
+      </div>
+    </div>
+  `;s.insertAdjacentHTML("beforeend",i);const n=s.querySelector(".feedback-slider");if(!n)return console.error("Swiper container not found");const t=new w(n,{spaceBetween:30,slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},on:{init(a){r(a)},slideChange(a){r(a)}}});document.querySelector(".swiper-button-next")?.addEventListener("click",()=>{t.slideNext()}),document.querySelector(".swiper-button-prev")?.addEventListener("click",()=>{t.slidePrev()});function r(a){const y=a.slides.length,m=a.activeIndex,c=a.el.querySelector(".bullet-left"),l=a.el.querySelector(".bullet-middle"),d=a.el.querySelector(".bullet-right");!c||!l||!d||(c.classList.remove("active"),l.classList.remove("active"),d.classList.remove("active"),m===0?c.classList.add("active"):m===y-1?d.classList.add("active"):l.classList.add("active"))}};function q(e){const s=Math.round(e),i=5-s;return'<span class="stars">'+'<span class="fa fa-star checked"></span>'.repeat(s)+'<span class="fa fa-star" style="color: #fff;"></span>'.repeat(i)+"</span>"}async function I(){let e;try{e=o.create(g.artistsList),o.show(e);const{data:s}=await E();o.show(e),$(s.artists)}catch(s){s.response?v.error({message:`Error ${s.response.status}: ${s.response.data}`}):v.error({message:`Error: ${s.message}`})}finally{o.hide(e)}}async function T(){try{const{feedbacks:e}=await k(1,10);if(!Array.isArray(e))throw new Error("Feedbacks is not an array");P(e)}catch(e){console.error("Feedback fetch error:",e)}}const p=document.getElementById("toTop");window.addEventListener("scroll",()=>{window.scrollY>600?p.classList.add("show"):p.classList.remove("show")});p.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});I();T();
 //# sourceMappingURL=index.js.map
